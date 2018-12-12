117f99bd294cad0331771379c27392365498e28b
Eduardo Cardoso <eduardo.gil.s.cardoso@gmail.com>
2018-12-12 11:36:44 -0300

    uhu

--- gerar.py
+++ gerar.py
@@ -3,7 +3,11 @@ import os
 from win32com import client
 from docx import Document
 
-def get_run(modelo):
+def get_run(modelo, n):
+
+    print(n)
+
+    runs = [0 for i in range(n)]
 
     for i in modelo.tables:
         x = len(i.columns)
@@ -13,27 +17,40 @@ def get_run(modelo):
             for k in range(y):
                 ps = i.cell(k, j).paragraphs
                 for l in ps:
-                    if l.runs and l.runs[0].text == 'Valor_1':
-                        return l.runs[0]
+                    for z in l.runs:
+                        #print(z.text)
+
+                        for w in range(n):
+                            if z.text == 'Valor_'+str(w+1): runs[w] = z
+
+                        #print(runs)
+                        if all(y != 0 for y in runs): return runs
 
 wdFormatPDF = 17
 
-print('> Modelo do Certificado')
+print('\n> Modelo do Certificado')
 file_path = os.path.abspath(input('\n< '))
 
-print('> Lista de valores')
+print('\n> Lista de valores')
 list_path = os.path.abspath(input('\n< '))
 
-print('> Pasta de sáida')
+print('\n> Pasta de sáida')
 out_path = os.path.abspath(input('\n< '))
 
 if '\\' != out_path[-1]: out_path+='\\'
 
-nomes = sorted([i.split(';')[0].strip('\n') for i in open(list_path).readlines()[1:]])
+nomes = sorted([i.split(';') for i in open(list_path).readlines()[1:]])[1:]
+
+
+for i in range(len(nomes)):
+    nomes[i] = [j.strip('\n') for j in nomes[i]]
+
+
 
 modelo = Document(file_path)
 
-run = get_run(modelo)
+
+runs = get_run(modelo,len(nomes[0]))
 
 word = client.Dispatch('Word.Application')
 
@@ -44,15 +61,17 @@ for i in nomes:
 
     print('> Gerano arquivo %d de %d' %(j,len(nomes)),end='\r')
     j+=1
-    run.text = i
 
-    modelo.save(out_path+i+'.docx')
+    for k in range(len(i)):
+        runs[k].text = i[k]
 
-    doc = word.Documents.Open(os.path.abspath(out_path+i + '.docx'))
-    doc.SaveAs(out_path+ i + '.pdf', FileFormat=wdFormatPDF)
+    modelo.save(out_path+i[0]+'.docx')
+
+    doc = word.Documents.Open(os.path.abspath(out_path+i[0] + '.docx'))
+    doc.SaveAs(out_path+ i[0] + '.pdf', FileFormat=wdFormatPDF)
     doc.Close()
 
-    os.remove(out_path+i+'.docx')
+    os.remove(out_path+i[0]+'.docx')
 
 
 word.Quit()
